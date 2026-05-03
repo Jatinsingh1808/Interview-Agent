b684b7da7471a14466d6246fed39eaab6c2585d5
Abhi21-7 <abhishekagnihotri2005@gmail.com>
2026-05-03 14:07:05 +0530

    Fix: Update Firebase credentials to correct project

--- client/src/utils/firebase.js
+++ client/src/utils/firebase.js
@@ -7,7 +7,8 @@ const firebaseConfig = {
   projectId: import.meta.env.VITE_FIREBASE_PROJECTID,
   storageBucket: import.meta.env.VITE_FIREBASE_STORAGEBUCKET,
   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig);
